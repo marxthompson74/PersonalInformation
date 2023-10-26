b649fbc12b3185d3fc202737f5fc8b3dc0d90378
Marx Thompson <marxthompson@yahoo.com>
2023-10-26 14:35:24 -0600

    First commit

---
+++ PersonalInformation/src/homework/Main.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String firstName = "Rose";
+		String lastName = "Minnow";
+		String streetAddress = "123 W Blank Rd";
+		String city = "Niagra Falls";
+		String zipCode = "14304";
+		
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + streetAddress);
+		System.out.println("City: " + city);
+		System.out.println("Zip Code: " + zipCode);
+
+	}
+
+}
